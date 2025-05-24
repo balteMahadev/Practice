dd66be092657500f147bbfa93867c7ab9b3f0d57
bmahadev <baltemahadev633@gmail.com>
2025-05-24 16:19:07 +0530

    Leaders in an Array: Identify all elements in an array that are greater than all # elements to their right. For example, in [16,17, 4, 3, 5, 2], the leaders are [17, 5, 2].

--- coding/basic-coding/leaders_in_the_list_from_right.py
+++ coding/basic-coding/leaders_in_the_list_from_right.py
@@ -1,2 +1,16 @@
 # Leaders in an Array: Identify all elements in an array that are greater than all
 # elements to their right. For example, in [16, 17, 4, 3, 5, 2], the leaders are [17, 5, 2].
+
+def find_leaders(list):
+    n = len(list)
+    leaders = []
+    max_from_right = list[-1]
+    leaders.append(max_from_right)
+    for i in range(n-2, -1, -1):
+        if list[i] > max_from_right:
+            leaders.append(list[i])
+            max_from_right = list[i]
+    return leaders[::-1]
+
+arr = [16, 17, 4, 3, 5, 2]
+print(find_leaders(arr))  # Output: [17, 5, 2]
